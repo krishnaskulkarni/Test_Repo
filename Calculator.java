f7113b156401e490ddf3217f815dc306e73fe94e
Bijapure <arif.bijapure@capgemini.com>
2019-03-04 15:10:11 +0530

    Edited Calculator.java file

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
 
          public static void main(String [] args){
                   System.out.println("Hello World!!!!");
-                  System.out.println("Calculator Java Program");
+                 
          }
 }

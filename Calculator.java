54ab8c8d3cce89505d20ae0c6d8e839218562731
krishnaskulkarni <48197276+krishnaskulkarni@users.noreply.github.com>
2019-03-05 15:51:57 +0530

    added usless content

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,9 @@ public class Calculator {
          public static void main(String [] args){
                   System.out.println("Hello World!!!!");
                   System.out.println("Calculator Java Program");
+           public static void fruits(String [] args){
+                  System.out.println("Hello World!!!!");
+                  System.out.println("nice fruits");
          }
+         
 }
